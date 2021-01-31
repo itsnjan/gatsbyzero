fb427809eba5e7067143921b448857739e9946dd
Jan <jan.leon.voelker@gmail.com>
2021-02-01 00:33:13 +0100

    minor changes in footer social media menu

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -23,9 +23,7 @@ const Footer = props => (
       <h2>Etiam feugiat</h2>
       <dl className="alt">
         <dt>Address</dt>
-        <dd>1234 Somewhere Road &bull; Nashville, TN 00000 &bull; USA</dd>
-        <dt>Phone</dt>
-        <dd>(000) 000-0000 x 0000</dd>
+        <dd>Berlin, Germany</dd>
         <dt>Email</dt>
         <dd>
           <a href="https://codebushi.com">information@untitled.tld</a>
@@ -33,34 +31,16 @@ const Footer = props => (
       </dl>
       <ul className="icons">
         <li>
-          <a
-            href="https://twitter.com/huntaroSan"
-            className="icon fa-twitter alt"
-          >
-            <span className="label">Twitter</span>
-          </a>
-        </li>
-        <li>
-          <a href="https://codebushi.com" className="icon fa-facebook alt">
-            <span className="label">Facebook</span>
-          </a>
-        </li>
-        <li>
-          <a href="https://codebushi.com" className="icon fa-instagram alt">
-            <span className="label">Instagram</span>
+          <a href="https://www.linkedin.com/in/jan-voelker-webdev/" className="icon fa-linkedin alt">
+            <span className="label">LinkedIn</span>
           </a>
         </li>
         <li>
           <a
-            href="https://github.com/codebushi/gatsby-starter-stellar"
+            href="https://github.com/jan-webdev"
             className="icon fa-github alt"
           >
             <span className="label">GitHub</span>
-          </a>
-        </li>
-        <li>
-          <a href="https://codebushi.com" className="icon fa-dribbble alt">
-            <span className="label">Dribbble</span>
           </a>
         </li>
       </ul>
